53e2c020d4bc3bf3baf195c6ded279d360e064cf
Olena Stasiuk <estasjuk@gmail.com>
2022-11-01 13:23:01 +0200

    add task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,14 @@
+const refs = {
+    input: document.querySelector("#name-input"),
+    nameLabel: document.querySelector("#name-output"),
+};
+
+refs.input.addEventListener("input", onInputChange);
+
+
+function onInputChange(event) {
+    refs.nameLabel.textContent = event.currentTarget.value;
+    if (refs.nameLabel.textContent === '') {
+        refs.nameLabel.textContent = 'Anonymous';
+    }
+}
