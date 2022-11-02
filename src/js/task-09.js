57878d7a6ede849f4a155192a068af3045fc78cb
Olena Stasiuk <estasjuk@gmail.com>
2022-11-03 00:46:19 +0200

    add task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+const refs = {
+  body: document.querySelector("body"),
+  changeColorBtn: document.querySelector(".change-color"),
+  colorOutput: document.querySelector(".color"),
+}
+
+refs.changeColorBtn.addEventListener("click", onChangeBodyColor);
+
+function onChangeBodyColor(event) {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorOutput.textContent = getRandomHexColor();
+ }
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
