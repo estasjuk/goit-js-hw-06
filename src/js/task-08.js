a9c2ce2c0c63f190587f7e7e69ecf34b3aef30f4
Olena Stasiuk <estasjuk@gmail.com>
2022-11-03 00:29:07 +0200

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector('.login-form');
+    
+formRef.addEventListener("submit", onSubmit);
 
+function onSubmit(event) {
+    event.preventDefault();
+
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    const formData = { email: email.value, password: password.value };
+    
+        if (email.value === "" || password.value === "") {
+        return alert("Все поля должны быть заполнены")
+    }
+    
+    console.log(formData);
+    event.currentTarget.reset();
+}
