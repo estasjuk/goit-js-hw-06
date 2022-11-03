850059f83c30476692ae98b49382e5e80ccf003a
Olena Stasiuk <estasjuk@gmail.com>
2022-11-04 00:10:51 +0200

    add removing antagonist-classes to the task 6

--- js/task-06.js
+++ js/task-06.js
@@ -5,8 +5,13 @@ inputRef.addEventListener("blur", onCheckAmountOfSymbols);
 
 function onCheckAmountOfSymbols(event) { 
     if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
-        event.currentTarget.classList.add("valid")
+        event.currentTarget.classList.add("valid");
+        event.currentTarget.classList.remove("invalid");
     }
-        else
-        { event.currentTarget.classList.add("invalid") }
+    else
+    
+    {
+        event.currentTarget.classList.remove("valid");
+        event.currentTarget.classList.add("invalid");
+    }
     }
