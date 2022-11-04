edf7e2f866ab6539d37fc22621b9597992a7d5b7
Olena Stasiuk <estasjuk@gmail.com>
2022-11-04 15:51:00 +0200

    add function for add/remove class to the task 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,15 +3,17 @@ const lengthOfInputRef = document.querySelector("#validation-input[data-length]"
 
 inputRef.addEventListener("blur", onCheckAmountOfSymbols);
 
+function changeClass(elem, remove, add) {
+  elem.classList.remove(remove);
+  elem.classList.add(add);
+}
+
 function onCheckAmountOfSymbols(event) { 
     if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
-        event.currentTarget.classList.add("valid");
-        event.currentTarget.classList.remove("invalid");
+        changeClass(inputRef, 'invalid', 'valid');
     }
     else
-    
-    {
-        event.currentTarget.classList.remove("valid");
-        event.currentTarget.classList.add("invalid");
+        {
+        changeClass(inputRef, 'valid', 'invalid');
     }
-    }
+}
